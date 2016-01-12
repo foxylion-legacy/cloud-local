78a5140070fa2962ee9a1d3d37bb82a625777ae2
Jakob Jarosch <dev@jakobjarosch.de>
2016-01-12 23:22:42 +0100

    Add basic version of docker image for mail

--- vagrant/provison-dev-vm.sh
+++ vagrant/provison-dev-vm.sh
@@ -4,10 +4,16 @@ apt-get update
 apt-get install -y software-properties-common make git
 
 # Install docker-engine
+# We use the linux-image-extra to enable AUFS storage driver which is much more performant
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
 apt-get update
-apt-get install -y docker-engine
+apt-get install -y linux-image-extra-$(uname -r) docker-engine
+gpasswd -a vagrant docker
+cat <<EOF > /etc/default/docker
+DOCKER_OPTS="--storage-driver=aufs"
+EOF
+service docker restart
 
 # Install ansible
 apt-get install software-properties-common
